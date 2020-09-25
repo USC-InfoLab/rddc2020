c8fca9b518cf76e9a35f9985b2edceacdc9c38d6
dweeptrivedi <dweeptrivedi1994@gmail.com>
2020-09-25 13:24:40 +0000

    add more scripts

---
+++ yolov5/scripts/download_IMSC_grddc2020_weights.sh
@@ -0,0 +1,5 @@
+gdown https://drive.google.com/uc?id=1F_0MHIBuO1wgVwePk6UAuFudKmCf_7Fs -O weights/IMSC/last_95_448_32_aug2.pt
+gdown https://drive.google.com/uc?id=1Fw6_ku3Z8aTdy4vwjZatHTkaNyeT7ZoZ -O weights/IMSC/last_95_640_16.pt
+gdown https://drive.google.com/uc?id=1Xu2KDBkD09E7ItOkKrodM_XzOQu-6Mhl -O weights/IMSC/last_95.pt
+gdown https://drive.google.com/uc?id=1ky9aZ1ygiy2qXlY_zcpj_4QI1ccfQTcE -O weights/IMSC/last_100_100_640_16.pt
+gdown https://drive.google.com/uc?id=1Wd1KA8j-q6qRQzy6ytLEav89xsmiqLFB -O weights/IMSC/last_120_640_32_aug2.pt
